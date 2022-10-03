aead8a81d915f71c96a66e445d074f80c2556324
Mark A SFF <mjagerton@ucdavis.edu>
2022-10-02 17:19:09 -0700

    tabs to spaces

--- src/matrix.jl
+++ src/matrix.jl
@@ -9,29 +9,29 @@ function mat_logsumexp_dual_reinterp!(
     Vbar::AbstractVector{D}, tmp_max::AbstractVector{V}, 
     tmpX::Matrix{V}, X::AbstractMatrix{D}
     ) where {T,V,K,D<:FD.Dual{T,V,K}}
-	
+    
     m,n = size(X)
 
     (m,n) == size(tmpX) || throw(DimensionMismatch())
     (m,) == size(Vbar) == size(tmp_max) || throw(DimensionMismatch())
 
     Vre   = reinterpret(reshape, V, Vbar)
-	Xre   = reinterpret(reshape, V, X)
+    Xre   = reinterpret(reshape, V, X)
 
     tmp_inv = tmp_max # resuse
 
-	fill!(Vbar, 0)
+    fill!(Vbar, 0)
     fill!(tmp_max, typemin(V))
 
     @turbo for i in 1:m, j in 1:n
         tmp_max[i] = max(tmp_max[i], Xre[1,i,j])
     end
 
-	@turbo for i in 1:m, j in 1:n
+    @turbo for i in 1:m, j in 1:n
         ex = exp(Xre[1,i,j] - tmp_max[i])
         tmpX[i,j] = ex
-		Vre[1,i] += ex
-	end
+        Vre[1,i] += ex
+    end
 
     @turbo for i in 1:m
         v = Vre[1,i]
@@ -41,8 +41,8 @@ function mat_logsumexp_dual_reinterp!(
     end
 
     @turbo for i in 1:m, j in 1:n, k in 1:K
-		Vre[k+1,i] += tmpX[i,j]*Xre[k+1,i,j]*tmp_inv[i]
-	end
+        Vre[k+1,i] += tmpX[i,j]*Xre[k+1,i,j]*tmp_inv[i]
+    end
 
     return Vbar
 
@@ -51,17 +51,17 @@ end
 "using base loops with LoopVectorization `exp` and `log`"
 function mat_logsumexp_vexp_log_fast!(Vbar, tmp_max, X)
     m,n = size(X)
-	maximum!(tmp_max, X)
-	fill!(Vbar, 0)
-	for j in 1:n
-		for i in 1:m
-			Vbar[i] += vexp(X[i,j] - tmp_max[i])
-		end
-	end
-	for i in 1:m
-		Vbar[i] = log_fast(Vbar[i]) + tmp_max[i]
-	end
-	return Vbar
+    maximum!(tmp_max, X)
+    fill!(Vbar, 0)
+    for j in 1:n
+        for i in 1:m
+            Vbar[i] += vexp(X[i,j] - tmp_max[i])
+        end
+    end
+    for i in 1:m
+        Vbar[i] = log_fast(Vbar[i]) + tmp_max[i]
+    end
+    return Vbar
 end
 
 """
@@ -70,9 +70,9 @@ using `LoopVectorization.@turbo` loops
 **NOTE** - not compatible with `ForwardDiff.Dual` numbers!
 """
 function mat_logsumexp_float_turbo!(Vbar::AbstractVector{T}, tmp_max::AbstractVector{T}, X::AbstractMatrix{T}) where {T<:AbstractFloat}
-	m,n = size(X)
+    m,n = size(X)
 
-	fill!(Vbar, 0)
+    fill!(Vbar, 0)
     fill!(tmp_max, typemin(T))
     
     @turbo for i in 1:m, j in 1:n
@@ -80,12 +80,12 @@ function mat_logsumexp_float_turbo!(Vbar::AbstractVector{T}, tmp_max::AbstractVe
     end
     
     @turbo for i in 1:m, j in 1:n
-		Vbar[i] += exp(X[i,j] - tmp_max[i])
-	end
-	
+        Vbar[i] += exp(X[i,j] - tmp_max[i])
+    end
+    
     @turbo for i in 1:m
-		Vbar[i] = log(Vbar[i]) + tmp_max[i]
-	end
-	return Vbar
+        Vbar[i] = log(Vbar[i]) + tmp_max[i]
+    end
+    return Vbar
 end
 
--- src/vector.jl
+++ src/vector.jl
@@ -4,47 +4,47 @@
 
 "fastest logsumexp over Dual vector requires tmp vector"
 function vec_logsumexp_dual_reinterp!(tmp::AbstractVector{V}, X::AbstractVector{<:FD.Dual{T,V,K}}) where {T,V,K}
-	Xre   = reinterpret(reshape, V, X)
+    Xre   = reinterpret(reshape, V, X)
 
     uv = typemin(V)
     @turbo for i in eachindex(X)
         uv = max(uv, Xre[1,i])
     end
 
-	s = zero(V)
+    s = zero(V)
 
     @turbo for j in eachindex(X,tmp)
-		ex = exp(Xre[1,j] - uv)
-		tmp[j] = ex
-		s += ex
-	end
+        ex = exp(Xre[1,j] - uv)
+        tmp[j] = ex
+        s += ex
+    end
 
-	v = log(s) + uv # logsumexp value
+    v = log(s) + uv # logsumexp value
 
-	invs = inv(s) # for doing softmax for derivatives
+    invs = inv(s) # for doing softmax for derivatives
 
-	# would be nice to use a more elegant consruction for
-	# pvec instead of multiple conversions below
-	# that said, it seems like we still have zero allocations
-	pvec = zeros(MVector{K,V})
-	@turbo for j in eachindex(X,tmp)
-		tmp[j] *= invs
-		for k in 1:K
-			pvec[k] += tmp[j]*Xre[k+1,j]
-		end
-	end
+    # would be nice to use a more elegant consruction for
+    # pvec instead of multiple conversions below
+    # that said, it seems like we still have zero allocations
+    pvec = zeros(MVector{K,V})
+    @turbo for j in eachindex(X,tmp)
+        tmp[j] *= invs
+        for k in 1:K
+            pvec[k] += tmp[j]*Xre[k+1,j]
+        end
+    end
 
-	ptup = NTuple{K,V}(pvec)
-	ptl = FD.Partials{K,V}(ptup)
+    ptup = NTuple{K,V}(pvec)
+    ptl = FD.Partials{K,V}(ptup)
 
-	return FD.Dual{T,V,K}(v, ptl)
+    return FD.Dual{T,V,K}(v, ptl)
 
 end
 
 "wrapper allocates tmp vector"
 function vec_logsumexp_dual_reinterp!(X::AbstractVector{<:FD.Dual{T,V,K}}) where {T,V,K}
-	tmp = Vector{V}(undef, length(X))
-	return vec_logsumexp_dual_reinterp!(tmp, X)
+    tmp = Vector{V}(undef, length(X))
+    return vec_logsumexp_dual_reinterp!(tmp, X)
 end
 
 "logsumexp with @turbo. maybe a bit less safe/stable... but REALLY fast!"
@@ -52,7 +52,7 @@ function vec_logsumexp_float_turbo!(x::AbstractVector{T}) where {T<:AbstractFloa
     n = length(x)
     u = maximum(x)                                       # max value used to re-center
     
-	s = zero(T)
+    s = zero(T)
     @turbo for i = 1:n
         tmp = exp(x[i] - u)
         s += tmp
